2c9f2297ba6c4b0d7fb2e27b5be456658ed7516d
iOlis-Lui <luizakisamba@mail.ru>
2023-12-21 19:35:03 +0300

    lesson-end

--- main.js
+++ main.js
@@ -1,24 +1,87 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+const domOutput = document.getElementById('output');
+const domInput = document.getElementById('input');
+const domButton = document.getElementById('enter');
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const todos = [];
 
-setupCounter(document.querySelector('#counter'))
+// LISTENERS METHODS
+const onCreateTodoButtonClick = function(event) {
+  console.log('domButton -> onclick: 0) - check input is not empty');
+
+  if (checkInputWithText()) {
+    todos.push(domInput.value);
+    renderTodos();
+    domInput.value = '';
+
+  }
+  else renderAlert('Wrong input');
+}
+const onInputTextInput = function() {
+  renderInputHighlightColor("");
+}
+const onTodoDeleteClick = function(event) {
+  console.log('todoDelete -> onTodoDeleteClick', event);
+  const btn = event.currentTarget;
+  const index = btn.dataset.index;
+  console.log('todoDelete -> onTodoDeleteClick', { index });
+  todos.splice(parseInt(index), 1);
+  renderTodos();
+}
+// LISTENERS ENDs
+
+// LOGICAL METHODS
+const checkInputWithText = () => domInput.value.length > 0 && isNaN(domInput.value);
+// LOGICAL ENDs
+
+// RENDER METHODS
+const renderAlert = function(message) {
+  renderInputHighlightColor("red");
+  alert(message);
+}
+const renderTodos = () => { 
+  domOutput.innerHTML = '';
+  console.log('renderTodos:', todos); 
+  for (let index = 0; index < todos.length; index++) { 
+    const todoItem = todos[index];   
+    console.log('renderTodos -> todoItem:', todoItem); 
+    renderTodo(index, todoItem);
+  } 
+}
+const renderInputTextInOutput = function() {
+  console.log('domButton -> onclick: 1) - add todo');
+
+  renderTodos(todos.length, domInput.value);
+
+  console.log('domButton -> onclick: 2) - clear input');
+  domInput.value = '';
+}
+const renderTodo = (index, text) => {
+  const domTodoContainer = document.createElement('div');
+
+  const domTodoButtonDelete = createActionButtonWithText('delete');
+  domTodoButtonDelete.dataset.index = index;
+  domTodoButtonDelete.onclick = onTodoDeleteClick;
+
+  domTodoContainer.innerText = `${index}. ${text}`;
+
+  domTodoContainer.appendChild(domTodoButtonDelete);
+  domOutput.appendChild(domTodoContainer);
+  
+}
+const renderInputHighlightColor = (color) => domInput.style.backgroundColor = color;
+// RENDER ENDs
+
+// UTILS Methods
+const createActionButtonWithText = (text) => {
+  const btn = document.createElement('button');
+  btn.innerText = text;
+  btn.style.backgroundColor = 'red';
+  btn.style.marginLeft = '12px';
+  btn.style.border = 'solid 2px';
+  btn.style.borderColor = 'green';
+  return btn;
+}
+// UTILS ENDs
+
+domButton.onclick = onCreateTodoButtonClick;
+domInput.oninput = onInputTextInput;
